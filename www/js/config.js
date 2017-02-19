404fe7082e898b357395a38b914cb19de384f9d2
Francesco <f.falanga7@studenti.unisa.it>
2017-02-19 17:31:32 +0100

    Creating fixed project

---
+++ www/js/config.js
@@ -0,0 +1,12 @@
+scientificConferenceApp={
+                            appname:"Scientific Conference",                                            // nome dell'applicazione
+                            backgroundColour:"white",                                                   // consente di cambiare il colore di sfondo del container del logo, va scelto usando rgb o un colore inglese
+                            serverIp:"35.166.114.130",                                                  // ip del server da cui scaricare i dati
+                            conferenceName:"Mobile app technologies",                                   // nome della conferenza
+                            logo:"logo.png",                                                            // nome del logo, presente nella cartella img
+                            conferenceInfo: "La conferenza riguarda la discussione "+                   // Descrizione generale del convegno
+                                "delle principali tecnologie, che consentono la re"+
+                                "alizzazione e progettazione di applicazioni ibride.",
+                            conferencePlace:"Via Giovanni Paolo II, 132 - 84084 - Fisciano (SA)",       // Luogo del convegno
+                            organizers:["Alessia Saggese", "Valentino Graziano"]                        // Organizzatori del convegno
+                        };
